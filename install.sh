44527c77d9634fb7ff568de0e206e6b10f4d0f26
David Koch <dmkoch3@gmail.com>
2017-11-29 22:14:52 -0500

    Finished checking PA download integrity Need to work on group creation next

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@
 # variables
 installdir=$(pwd)
 pa_ver_req=5.0
+pa_ver_latest=11.1
 be_ver_req=5.23
 install_pa=false
 install_be=false
@@ -38,7 +39,7 @@ then
 	echo 'PuleAudio is not installed'
 	$install_pa=true
 else
-	pa_ver=$(pulseaudio --version)
+	pa_ver=$(pulseaudio --version | sed 's/pulseaudio //')
 	# Version Checking code
 	# Source: https://stackoverflow.com/questions/4023830/how-to-compare-two-strings-in-dot-separated-version-format-in-bash
 	if [ $pa_ver == $pa_ver_req ]
@@ -61,21 +62,46 @@ fi
 # Install PulseAudio if required
 if [ $install_pa = true ]
 then
-	echo 'Installing PulseAudio 5.0'
-	echo 'You my wish to upgrade after the install'
+	echo "Installing PulseAudio ${pa_ver_latest}"
 	cd ~
-	wget : http://freedesktop.org/software/pulseaudio/releases/pulseaudio-5.0.tar.xz
-	tar xvf pulseaudio-5.0.tar.xz
-	cd pulseaudio-5.0/
+	
+	# Below line retrieves code for minimum required version according to source document at the top of the file
+	# Making this code on latest libraries fails
+	# wget : http://freedesktop.org/software/pulseaudio/releases/pulseaudio-5.0.tar.xz
+	# Below line retrieves latest source code according to variable at top of file
+	# How to detect updates and get newer code?
+	wget : "https://freedesktop.org/software/pulseaudio/releases/pulseaudio-${pa_ver_latest}.tar.xz"
+	
+	# Need to verify download file using .sha1 file for downloaded file
+	wget : "https://freedesktop.org/software/pulseaudio/releases/pulseaudio-${pa_ver_latest}.tar.xz.sha1"
+	if [ $(sha1sum pulseaudio-${pa_ver_latest}.tar.xz) != $(cat pulseaudio-${pa_ver_latest}.tar.xz.sha1) ]
+	then
+		echo 'Download was corrupted, aborting install'
+		exit 0
+	else
+		echo 'Verified download integrity'
+	fi
+
+	echo 'Extracting archive...'
+	tar xvf "pulseaudio-${pa_ver_latest}.tar.xz"
+	
+	echo "Configuring and installing PulseAudio ${pa_ver_latest}"
+	cd "pulseaudio-${pa_ver_latest}/"
 	./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --disable-bluez4 --disable-rpath --with-module-dir=/usr/lib/pulse/modules
 	make
 	sudo make install
-	echo 'Creating pulse and audio users to run PulseAudio in system-wide mode
-	sudo addgroup --system pulse
-	sudo adduser --system --ingroup pulse --home /var/run/pulse pulse
-	sudo addgroup --system pulse-access
-	sudo adduser pulse audio
-	echo 'Adding root to pulse-access group'
-	sudo adduser root pulse-access
+	echo "PulseAudio ${pa_ver_latest} installed" 
+fi
+
+# Client should have PulseAudio installed at this point
+# Create necessary groups whether or not PulseAudio installed for bluePi
+# Need to check for existing groups/users
+echo 'Creating pulse and audio users to run PulseAudio in system-wide mode
+sudo addgroup --system pulse
+sudo adduser --system --ingroup pulse --home /var/run/pulse pulse
+sudo addgroup --system pulse-access
+sudo adduser pulse audio
+echo 'Adding root to pulse-access group'
+sudo adduser root pulse-access
 	
 
