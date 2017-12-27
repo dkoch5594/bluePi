8986589ff24f851c6fb55a0af4786face35300dd
David Koch <dmkoch3@gmail.com>
2017-12-26 20:47:23 -0500

    Completed __init__ of BluePlayer class

--- src/bluePi.py
+++ src/bluePi.py
@@ -28,12 +28,66 @@ LOG_LEVEL = logging.DEBUG
 LOG_FILE = "/dev/stdout"
 LOG_FORMAT = "%(asctime)s %(levelname)s %(message)s"
 
+def getManagedObjects():
+	bus = dbus.SystemBus()
+	manager = dbus.Interface(bus.get_object(SERVICE_NAME, "/"), "org.freedesktop.DBus.ObjectManager")
+	return manager.GetManagedObjects()
+
+def findAdapter():
+	objects= getManagedObjects()
+	bus = dbus.SystemBus()
+	for path, ifaces in objects.iteritems():
+		adapter = ifaces.get(ADAPTER_IFACE)
+		if adapter is None:
+			continue
+		obj = bus.get_object(SERVICE_NAME, path)
+		return dbus.Interface(obj, ADAPTER_IFACE)
+	raise Exception("Bluetooth adapter not found")
+
 class BluePlayer(dbus.service.Object):
 	AGENT_PATH = "/blueplayer/agent"
 	#Unit does not have display
 	#Need to determine acutal capability
 	CAPABILITY = "DisplayOnly"
 	
+	bus = None
+	adapter = None
+	device = None
+	deviceAlias = None
+	player = None
+	transport = None
+	connected = None
+	state = None
+	status = None
+	discoerable = None
+	track = None
+	
+	def __init__(self):
+		# Initialize gobject and find any current media players
+		self.bus = dbus.SystemBus()
+
+		dbus.service.Object.__init__(self, self.bus(), BluePlayer.AGENT_PATH)
+
+		self.bus.add_signal_receiver(self.playerHandler,
+			bus_name = SERVICE_NAME,
+			dbus_interface = "org.freedesktop.DBus.Properties",
+			signal_name = "PropertiesChanged",
+			path_keyword = "path")
+
+		self.registerAgent()
+
+		adapter_path = findAdapter().object_path
+		self.bus.add_signal_receiver(self.adapterHandler,
+			bus_name = SERVICE_NAME,
+			path = adapter_path,
+			dbus_interface = "org.freedesktop.DBus.Properties",
+			signal_name = "PropertiesChanged",
+			path_keyword = "path")
+
+		self.findPlayer()
+
+	
+"""
 if __name__ == "__main__":
 	logging.basicConfig(filename=LOG_FILE, format=LOG_FORMAT, level=LOG_LEVEL)
 	logging.info("Starting BluePlayer")
@@ -53,3 +107,4 @@ if __name__ == "__main__":
 	#finally:
 	#	if player:
 	#		player.shutdown()
+"""
