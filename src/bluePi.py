8aa51069e773037782ed71aaba5b27177a1c30de
David Koch <dmkoch3@gmail.com>
2018-01-15 12:02:50 -0500

    Completely redid bluePi. Now connects to Trusted devices and allows new devices to connect.

--- src/bluePi.py
+++ src/bluePi.py
@@ -1,110 +1,176 @@
 #!/usr/bin/python
 
-# Source:
-# https://www.raspberrypi.org/forums/viewtopic.php?p=619713#p619713
-
-# Dependencies:
-# sudo apt-get install -y python-gobject
-# sudo apt-get install -y python-smbus
-# sudo apt-get install -y python-dbus
-
-#import signal
-import dbus
 import dbus
 import dbus.service
 import dbus.mainloop.glib
 import gobject
 import logging
+import time
 
-SERVICE_NAME = "org.bluez"
-AGENT_IFACE = SERVICE_NAME + ".Agent1"
-ADAPTER_IFACE = SERVICE_NAME + ".Adapter1"
-DEVICE_IFACE = SERVICE_NAME + ".Device1"
-PLAYER_IFACE = SERVICE_NAME + ".MediaPlayer1"
-TRANSPORT_IFACE = SERVICE_NAME + ".MediaTransport1"
-
-#LOG_LEVEL = logging.INFO
-LOG_LEVEL = logging.DEBUG
+# Variables
+#LOG_LEVEL = logging.DEBUG
+LOG_LEVEL = logging.INFO
 LOG_FILE = "/dev/stdout"
+#LOG_FILE = "path/to/some/log/file"
 LOG_FORMAT = "%(asctime)s %(levelname)s %(message)s"
 
-def getManagedObjects():
-	bus = dbus.SystemBus()
-	manager = dbus.Interface(bus.get_object(SERVICE_NAME, "/"), "org.freedesktop.DBus.ObjectManager")
-	return manager.GetManagedObjects()
+SERVICE_NAME = "org.bluez"
 
-def findAdapter():
-	objects= getManagedObjects()
-	bus = dbus.SystemBus()
-	for path, ifaces in objects.iteritems():
-		adapter = ifaces.get(ADAPTER_IFACE)
-		if adapter is None:
-			continue
-		obj = bus.get_object(SERVICE_NAME, path)
-		return dbus.Interface(obj, ADAPTER_IFACE)
-	raise Exception("Bluetooth adapter not found")
+IFACES = {"AGENT" : SERVICE_NAME + ".Agent1",
+	"AGENT_MANAGER" : SERVICE_NAME + ".AgentManager1",
+	"ADAPTER" : SERVICE_NAME + ".Adapter1",
+	"DEVICE" : SERVICE_NAME + ".Device1",
+	"MEDIA_PLAYER" : SERVICE_NAME + ".MediaPlayer1",
+	"MEDIA_TRANSPORT" : SERVICE_NAME + ".MediaTransport1" }
+FD_DBUS = "org.freedesktop.DBus"
+FD_PROPERTIES = FD_DBUS + ".Properties"
 
-class BluePlayer(dbus.service.Object):
-	AGENT_PATH = "/blueplayer/agent"
-	#Unit does not have display
-	#Need to determine acutal capability
-	CAPABILITY = "DisplayOnly"
-	
-	bus = None
-	adapter = None
-	device = None
-	deviceAlias = None
-	player = None
-	transport = None
-	connected = None
-	state = None
-	status = None
-	discoerable = None
-	track = None
-	
+class TestPlayer(dbus.service.Object):
+
 	def __init__(self):
-		# Initialize gobject and find any current media players
+		# Initialize bus
 		self.bus = dbus.SystemBus()
+		self.untrusted_devices = []
+		# Retrieve adapter, properties and interface
+		self.adapter_obj = self.getBusObject("ADAPTER")
+		self.adapter_props = dbus.Interface(self.adapter_obj, FD_PROPERTIES)
+		self.adapter = dbus.Interface(self.adapter_obj, IFACES["ADAPTER"])
+		logging.info("Found adapter at {}".format(self.adapter_obj.object_path))
 
-		dbus.service.Object.__init__(self, self.bus(), BluePlayer.AGENT_PATH)
+		# Initialize device handles
+		self.dev_obj = None
+		self.dev_props = None
+		self.device = None
+		
+		# Register signalHanlder on the bus
+		self.bus.add_signal_receiver(self.propertyChangeHandler
+			, bus_name = SERVICE_NAME
+			, dbus_interface = FD_PROPERTIES
+			, signal_name = "PropertiesChanged"
+			, path_keyword = "path")
+		logging.info("Registered propertyChangeHandler")
+		
+		self.bus.add_signal_receiver(self.interfaceAddedHandler
+			, bus_name = SERVICE_NAME
+			,dbus_interface = FD_DBUS + ".ObjectManager"
+			, signal_name = "InterfacesAdded")
+		logging.info("Registered interfaceAddedHandler")
 
-		self.bus.add_signal_receiver(self.playerHandler,
-			bus_name = SERVICE_NAME,
-			dbus_interface = "org.freedesktop.DBus.Properties",
-			signal_name = "PropertiesChanged",
-			path_keyword = "path")
+		# Turn adapter on
+		self.adapter_props.Set(IFACES["ADAPTER"], "Powered", True)
+		self.adapter_props.Set(IFACES["ADAPTER"], "Discoverable", True)
 
-		self.registerAgent()
-
-		adapter_path = findAdapter().object_path
-		self.bus.add_signal_receiver(self.adapterHandler,
-			bus_name = SERVICE_NAME,
-			path = adapter_path,
-			dbus_interface = "org.freedesktop.DBus.Properties",
-			signal_name = "PropertiesChanged",
-			path_keyword = "path")
-
-		self.findPlayer()
+		self.findSource()
 
 	
-"""
-if __name__ == "__main__":
-	logging.basicConfig(filename=LOG_FILE, format=LOG_FORMAT, level=LOG_LEVEL)
-	logging.info("Starting BluePlayer")
+	def connectToSource(self, path, interfaces):
+		success = False
+		if IFACES["DEVICE"] not in interfaces:
+			return success
 
+		dev_obj = self.bus.get_object(SERVICE_NAME, path)
+		dev_props = dbus.Interface(dev_obj, FD_PROPERTIES)
+		if dev_props.Get(IFACES["DEVICE"], "Trusted") == False:
+			return success
+
+		device = dbus.Interface(dev_obj, IFACES["DEVICE"])
+		try:
+			device.Connect()
+			success = True
+			return success
+		except dbus.exceptions.DBusException as ex:
+			logging.error("Error [{}] while trying to connect to {}".format(ex, path))
+			success = False
+			return success
+
+	def findSource(self):
+		foundSource = False
+		objects = self.getManagedObjects()
+		for path, ifaces in objects.iteritems():
+			foundSource = self.connectToSource(path, ifaces)
+			if foundSource == True:
+				break
+
+		if foundSource == False:
+			# Have iterated through existing devices, unable to connect to any
+			# Start discovery
+			self.adapter.StartDiscovery()	
+
+	def getBusObject(self, name):
+		objects = self.getManagedObjects()
+		for path, ifaces in objects.iteritems():
+			if IFACES[name] in ifaces:
+				return self.bus.get_object(SERVICE_NAME, path)
+
+	def getManagedObjects(self):
+		obj_manager_obj = self.bus.get_object(SERVICE_NAME, "/")
+		obj_manager = dbus.Interface(obj_manager_obj, FD_DBUS + ".ObjectManager")
+		return obj_manager.GetManagedObjects()
+
+	def interfaceAddedHandler(self, path, interfaces):
+		logging.debug("Interface added at {}".format(path))
+		self.connectToSource(path, interfaces)
+
+	def propertyChangeHandler(self, interface, changed, invalidated, path):
+		if interface == IFACES["DEVICE"]:
+			if "Paired" in changed:
+				if changed["Paired"] == True:
+					logging.info("Paired with device at {}".format(path))
+					dev_obj = self.bus.get_object(SERVICE_NAME, path)
+					dev_props = dbus.Interface(dev_obj, FD_PROPERTIES)
+					dev_props.Set(interface, "Trusted", True)
+			if "Connected" in changed:
+				if changed["Connected"] == True:
+					if self.adapter_props.Get(IFACES["ADAPTER"], "Discovering") == True:
+						self.adapter.StopDiscovery()
+					self.dev_obj = self.bus.get_object(SERVICE_NAME, path)
+					self.dev_props = dbus.Interface(self.dev_obj, FD_PROPERTIES)
+					self.device = dbus.Interface(self.dev_obj, IFACES["DEVICE"])
+					logging.info("Connected to device at {}".format(self.dev_obj.object_path))
+				elif changed["Connected"] == False:
+					logging.info("Disconnected from {}".format(self.dev_obj.object_path))
+					self.dev_obj = None
+					self.dev_props = None
+					self.device = None
+					self.findSource()
+		elif interface == IFACES["ADAPTER"]:
+			if "Powered" in changed:
+				if changed["Powered"] == True:
+					logging.info("Adapter powered on.")
+			if "Discoverable" in changed:
+				if changed["Discoverable"] == True:
+					logging.info("Adapter is discoverable")
+				elif changed["Discoverable"] == False:
+					logging.info("Adapter is no loger discoverable")
+			if "Discovering" in changed:
+				if changed["Discovering"] == True:
+					logging.info("Starting discovery")
+				elif changed["Discovering"] == False:
+					logging.info("Discovery Stopped")
+	
+	def shutdown(self):
+		logging.info("Shutting down bluePlayer service")
+		if self.adapter_props.Get(IFACES["ADAPTER"], "Discovering") == True:
+			self.adapter.StopDiscovery()
+		if self.device and self.dev_props.Get(IFACES["DEVICE"], "Connected") == True:
+			self.device.Disconnect()
+		self.adapter_props.Set(IFACES["ADAPTER"], "Discoverable", False)
+		self.adapter_props.Set(IFACES["ADAPTER"], "Powered", False)
+
+if __name__ == "__main__":
+	# Configure Logging
+	logging.basicConfig(filename=LOG_FILE, format=LOG_FORMAT, level=LOG_LEVEL)
+	
+	# Set main loop as default (necessary for asynch calls/signals) 
 	gobject.threads_init()
 	dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-
-	player = None
+	
+	tp = None
 	try:
-		player = BluePlayer()
+		tp = TestPlayer()
 		mainloop = gobject.MainLoop()
 		mainloop.run()
-	except KeyboardInterrupt as ex:
-		logging.info("BluePlayer canceled by user")
-	except Exception as ex:
-		logging.error("Unhandled error: {}".format(ex))
-	#finally:
-	#	if player:
-	#		player.shutdown()
-"""
+	except KeyboardInterrupt:
+		if tp:
+			tp.shutdown()
+
