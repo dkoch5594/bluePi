2256259e2e1ca36d580fd3541e243b728339a14d
David Koch <dmkoch3@gmail.com>
2018-02-07 20:06:36 -0500

    Renamed BLUEZ and FREEDESKTOP variables to make way for OFONO service variables.

--- bluePi.py
+++ bluePi.py
@@ -8,22 +8,28 @@ import logging
 import time
 
 # Variables
-#LOG_LEVEL = logging.DEBUG
-LOG_LEVEL = logging.INFO
+LOG_LEVEL = logging.DEBUG
+#LOG_LEVEL = logging.INFO
 LOG_FILE = "/dev/stdout"
 #LOG_FILE = "path/to/some/log/file"
 LOG_FORMAT = "%(asctime)s %(levelname)s %(message)s"
 
-SERVICE_NAME = "org.bluez"
-
-IFACES = {"AGENT" : SERVICE_NAME + ".Agent1",
-	"AGENT_MANAGER" : SERVICE_NAME + ".AgentManager1",
-	"ADAPTER" : SERVICE_NAME + ".Adapter1",
-	"DEVICE" : SERVICE_NAME + ".Device1",
-	"MEDIA_PLAYER" : SERVICE_NAME + ".MediaPlayer1",
-	"MEDIA_TRANSPORT" : SERVICE_NAME + ".MediaTransport1" }
+BLUEZ_SERVICE = "org.bluez"
 FD_DBUS = "org.freedesktop.DBus"
-FD_PROPERTIES = FD_DBUS + ".Properties"
+OFONO_SERVICE = "org.ofono"
+
+IFACES = {"AGENT" : BLUEZ_SERVICE + ".Agent1",
+	"AGENT_MANAGER" : BLUEZ_SERVICE+ ".AgentManager1",
+	"ADAPTER" : BLUEZ_SERVICE + ".Adapter1",
+	"DEVICE" : BLUEZ_SERVICE + ".Device1",
+	"MEDIA_PLAYER" : BLUEZ_SERVICE + ".MediaPlayer1",
+	"MEDIA_TRANSPORT" : BLUEZ_SERVICE + ".MediaTransport1", 
+	"PROPERTIES" : FD_DBUS + ".Properties" }
+
+UUID_BASE = "-0000-1000-8000-00805f9b34fb"
+HSP_AG_UUID = "00001112" + UUID_BASE
+HFP_HAG_UUID = "0000111f" + UUID_BASE
+A2DP_SOURCE_UUID = "0000110a" + UUID_BASE
 
 class TestPlayer(dbus.service.Object):
 
@@ -33,7 +39,7 @@ class TestPlayer(dbus.service.Object):
 		self.untrusted_devices = []
 		# Retrieve adapter, properties and interface
 		self.adapter_obj = self.getBusObject("ADAPTER")
-		self.adapter_props = dbus.Interface(self.adapter_obj, FD_PROPERTIES)
+		self.adapter_props = dbus.Interface(self.adapter_obj, IFACES["PROPERTIES"])
 		self.adapter = dbus.Interface(self.adapter_obj, IFACES["ADAPTER"])
 		logging.info("Found adapter at {}".format(self.adapter_obj.object_path))
 
@@ -42,17 +48,18 @@ class TestPlayer(dbus.service.Object):
 		self.dev_props = None
 		self.device = None
 		
-		# Register signalHanlder on the bus
+		# Register propertyChangeHandler on the bus
 		self.bus.add_signal_receiver(self.propertyChangeHandler
-			, bus_name = SERVICE_NAME
-			, dbus_interface = FD_PROPERTIES
+			, bus_name = BLUEZ_SERVICE
+			, dbus_interface = IFACES["PROPERTIES"]
 			, signal_name = "PropertiesChanged"
 			, path_keyword = "path")
 		logging.info("Registered propertyChangeHandler")
 		
+		# Register interfaceAddedHandler on the bus
 		self.bus.add_signal_receiver(self.interfaceAddedHandler
-			, bus_name = SERVICE_NAME
-			,dbus_interface = FD_DBUS + ".ObjectManager"
+			, bus_name = BLUEZ_SERVICE
+			, dbus_interface = FD_DBUS + ".ObjectManager"
 			, signal_name = "InterfacesAdded")
 		logging.info("Registered interfaceAddedHandler")
 
@@ -68,14 +75,16 @@ class TestPlayer(dbus.service.Object):
 		if IFACES["DEVICE"] not in interfaces:
 			return success
 
-		dev_obj = self.bus.get_object(SERVICE_NAME, path)
-		dev_props = dbus.Interface(dev_obj, FD_PROPERTIES)
+		dev_obj = self.bus.get_object(BLUEZ_SERVICE, path)
+		dev_props = dbus.Interface(dev_obj, IFACES["PROPERTIES"])
 		if dev_props.Get(IFACES["DEVICE"], "Trusted") == False:
 			return success
 
 		device = dbus.Interface(dev_obj, IFACES["DEVICE"])
 		try:
 			device.Connect()
+			#device.ConnectProfile(HFP_HAG_UUID)
+			#device.ConnectProfile(A2DP_SOURCE_UUID)
 			success = True
 			return success
 		except dbus.exceptions.DBusException as ex:
@@ -100,15 +109,14 @@ class TestPlayer(dbus.service.Object):
 		objects = self.getManagedObjects()
 		for path, ifaces in objects.iteritems():
 			if IFACES[name] in ifaces:
-				return self.bus.get_object(SERVICE_NAME, path)
+				return self.bus.get_object(BLUEZ_SERVICE, path)
 
 	def getManagedObjects(self):
-		obj_manager_obj = self.bus.get_object(SERVICE_NAME, "/")
+		obj_manager_obj = self.bus.get_object(BLUEZ_SERVICE, "/")
 		obj_manager = dbus.Interface(obj_manager_obj, FD_DBUS + ".ObjectManager")
 		return obj_manager.GetManagedObjects()
 
 	def interfaceAddedHandler(self, path, interfaces):
-		logging.debug("Interface added at {}".format(path))
 		self.connectToSource(path, interfaces)
 
 	def propertyChangeHandler(self, interface, changed, invalidated, path):
@@ -116,19 +124,19 @@ class TestPlayer(dbus.service.Object):
 			if "Paired" in changed:
 				if changed["Paired"] == True:
 					logging.info("Paired with device at {}".format(path))
-					dev_obj = self.bus.get_object(SERVICE_NAME, path)
-					dev_props = dbus.Interface(dev_obj, FD_PROPERTIES)
+					dev_obj = self.bus.get_object(BLUEZ_SERVICE, path)
+					dev_props = dbus.Interface(dev_obj, IFACES["PROPERTIES"])
 					dev_props.Set(interface, "Trusted", True)
 			if "Connected" in changed:
 				if changed["Connected"] == True:
 					if self.adapter_props.Get(IFACES["ADAPTER"], "Discovering") == True:
 						self.adapter.StopDiscovery()
-					self.dev_obj = self.bus.get_object(SERVICE_NAME, path)
-					self.dev_props = dbus.Interface(self.dev_obj, FD_PROPERTIES)
+					self.dev_obj = self.bus.get_object(BLUEZ_SERVICE, path)
+					self.dev_props = dbus.Interface(self.dev_obj, IFACES["PROPERTIES"])
 					self.device = dbus.Interface(self.dev_obj, IFACES["DEVICE"])
 					logging.info("Connected to device at {}".format(self.dev_obj.object_path))
 				elif changed["Connected"] == False:
-					logging.info("Disconnected from {}".format(self.dev_obj.object_path))
+					logging.info("Disconnected from {}".format(path))
 					self.dev_obj = None
 					self.dev_props = None
 					self.device = None
